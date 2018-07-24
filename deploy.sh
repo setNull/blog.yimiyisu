abf208521dabd22c6c8fc8e76d82acee084af35d
Jie Wang <759587515@qq.com>
2018-07-24 13:32:26 +0800

    rebuilding site Tue Jul 24 13:32:26 CST 2018

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+des="public"
+msg="rebuilding site `date`"
+if [ $# -eq 1  ]
+    then msg="$1"
+fi
+
+git add -A
+git commit -m "$msg"
+git push origin master
+
+# Build the project.
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
+hugo-algolia --config algolia.yaml
+
+#cp -r public/* $des
+# Go To Public folder
+cd $des
+
+rm -f algolia.json
+
+git add -A
+git commit -m "$msg"
+git push origin master
+
